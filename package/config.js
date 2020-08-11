86c0090874b2e20a62c36c8d172da6437d4d288d
Joel Moss <joel@developwithstyle.com>
2020-08-11 12:22:31 +0100

    fix: Ignore arbitrary output when fetch config from Rails runner

--- package/config.js
+++ package/config.js
@@ -1,7 +1,10 @@
 const spawnSync = require("child_process").spawnSync;
 const path = require("path");
 
-const configFromRails = railsRun("print Frails.config_as_json");
+const ioDelimiter = "__FRAILS_RAILS_RUNNER_DELIMETER__";
+const configFromRails = railsRun(
+  `puts "${ioDelimiter}#{Frails.config_as_json}${ioDelimiter}"`
+);
 const rootPath = process.cwd();
 
 module.exports = {
@@ -20,10 +23,18 @@ function railsRun(argument) {
   const result = spawnSync("./bin/rails", ["runner", argument]);
 
   if (result.status === 0) {
-    return JSON.parse(result.stdout.toString());
+    // Output is delimited to filter out unwanted warnings or other output
+    // that we don't want in our files.
+    const sourceRegex = new RegExp(ioDelimiter + "([\\s\\S]+)" + ioDelimiter);
+    const matches = result.stdout.toString().match(sourceRegex);
+    if (matches) {
+      return JSON.parse(matches[1]);
+    } else {
+      throw "Rails runner failed having been unable to parse the output.";
+    }
   } else if (result.signal !== null) {
     throw `Rails runner was terminated with signal: ${result.signal}`;
   } else {
-    throw `Rails runner failed with '${result.error.toString()}'.`;
+    throw `Rails runner failed with '${result.stderr.toString()}'.`;
   }
 }
